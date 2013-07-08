735e22598f35a51186bcc6c83d3b446c04239e57
Mike Higgins <mrmikehiggins@gmail.com>
2013-07-08 14:12:41 +0000

    Now reads file and outputs contents to response

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+
 var express = require('express');
+var fs = require('fs');
+var inputfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ var filecontents = fs.readFileSync(inputfile);
+ response.write(filecontents);
+
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
